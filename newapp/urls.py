6eaf79688bd851051e015d787ecb2eb94da2d70f
IngenieroEdu <jose.verdugo@cesunbc.edu.mx>
2026-02-22 23:13:14 -0800

    Update urls.py

--- newapp/urls.py
+++ newapp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("addrec/",views.addrec,name="addrec"),
     path('delete/<int:id>/',views.delete,name="delete"),
     path('update/<int:id>/',views.update,name="update"),
-    path('update/uprec/<int:id>/',views.uprec,name="uprec")
+    path('update/uprec/<int:id>/',views.uprec,name="uprec"),
+    path('', include('newapp.urls'))
+
 ]
